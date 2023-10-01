ff2c9f35aa5b0e079df8f275d5d7359fd0bd9545
rojassamson <rojas.samson.ignacio@outlook.com>
2023-10-01 00:06:21 -0300

    tercera preentrega Ignacio Rojas Samson

--- scripts/script.js
+++ scripts/script.js
@@ -1,124 +1,191 @@
-const productoPrecioBruto = new Map ([
-    ["Leche",1000],
+const productoPrecioBruto = new Map([
+    ["Leche", 1000],
     ["Mantequilla", 1400],
-    ["Jamón",2100],
+    ["Jamón", 2100],
     ["Galletas", 900],
     ["Bebida", 1200],
     ["Jugo", 1000],
     ["Mermelada", 800],
     ["Paté", 700]
 ]);
+
 const carritoCompras = new Map();
-cantidadProductos = 0;
-totalNeto = 0
-totalBruto = 0;
-iva = 0;
-listaProductos = 
-`
-Leche:      $1000
-Mantequilla:$1400
-Jamón:      $2100
-Galletas:   $900
-Bebida:     $1200
-Jugo:       $1000
-Mermelada:  $800
-Paté:       $700
-`
+const listaProductosElement = document.getElementById('listaProductos');
+const carritoListaElement = document.getElementById('carritoLista');
+const totalElement = document.getElementById('total');
+const verProductosButton = document.getElementById('verProductos');
+const productosListaElement = document.getElementById('productosLista');
+
+verProductosButton.addEventListener('click', mostrarProductos);
+
+function mostrarProductos() {
+    productosListaElement.style.display = 'block';
+    verProductosButton.style.display = 'none';
+
+    productoPrecioBruto.forEach((precio, producto) => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            ${producto}: $${precio}
+            <input type="number" id="cantidad-${producto}" placeholder="Cantidad" min="1">
+            <button data-producto="${producto}">Agregar al Carrito</button>
+        `;
+        
+        const addButton = listItem.querySelector('button');
+        addButton.addEventListener('click', agregarAlCarrito);
+
+        listaProductosElement.appendChild(listItem);
+    });
+}
+
+function agregarAlCarrito(event) {
+    const producto = event.target.getAttribute('data-producto');
+    const cantidadInput = document.getElementById(`cantidad-${producto}`);
+    const cantidad = parseInt(cantidadInput.value);
+
+    if (!isNaN(cantidad) && cantidad > 0) {
+        carritoCompras.set(producto, cantidad);
+        cantidadInput.value = ''; // Limpiar el campo de cantidad
+        actualizarCarrito();
+    }
+}
+
+function actualizarCarrito() {
+    carritoListaElement.innerHTML = '';
+
+    carritoCompras.forEach((cantidad, producto) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${producto} x${cantidad}`;
+        carritoListaElement.appendChild(listItem);
+    });
+
+    calcularTotal();
+}
+
+function calcularTotalPedido() {
+    let totalBruto = 0;
+
+    carritoCompras.forEach((cantidad, producto) => {
+        totalBruto += cantidad * productoPrecioBruto.get(producto);
+    });
+
+    const iva = totalBruto * 0.19;
+    const totalNeto = totalBruto - iva;
+
+    return {
+        totalBruto,
+        iva,
+        totalNeto
+    };
+}
+const carrito = document.getElementById('carrito');
+const confirmarPedidoButton = document.getElementById('confirmarPedido');
+
+verProductosButton.addEventListener('click', mostrarProductos);
+
+const pedidoConfirmadoElement = document.getElementById('pedidoConfirmado');
+const totalBrutoConfirmadoElement = document.getElementById('totalBrutoConfirmado');
+const ivaConfirmadoElement = document.getElementById('ivaConfirmado');
+const totalNetoConfirmadoElement = document.getElementById('totalNetoConfirmado');
 
 
+confirmarPedidoButton.addEventListener('click', () => {
+    const totalPedido = calcularTotalPedido();
 
-function llenarCarrito(producto, cantidad) {
-    carritoCompras.set(producto, cantidad);
-    let seguirComprando = confirm("¿Desea llevar otro producto?");
+    totalBrutoConfirmadoElement.textContent = `$${totalPedido.totalBruto}`;
+    ivaConfirmadoElement.textContent = `$${totalPedido.iva}`;
+    totalNetoConfirmadoElement.textContent = `$${totalPedido.totalNeto}`;
 
-    if (seguirComprando) {
-        let valida = false;
+    guardarCarritoEnLocalStorage();
+    mostrarPedidoConfirmado();
+});
 
-        while(valida == false) {
-            producto1 = prompt(listaProductos + "\n¿Que desea llevar? (Escriba el nombre del producto tal cómo aparece en el listado.)");
-            validaProducto = parseFloat(producto1);
-            if(isNaN(validaProducto) && productoPrecioBruto.has(producto1)) {
-                valida = true;
-            } else {
-                alert("Ingrese el nombre del producto que desea llevar, tal como aparece en el listado. Presione 'Aceptar' para mostrar el listado.");
-            }
-        }
+function mostrarPedidoConfirmado() {
+    pedidoConfirmadoElement.style.display = 'block';
+    carritoCompras.clear();
+    actualizarCarrito();
+}
 
-        valida = false;
-        while(valida == false){
-            cantidad1 = parseInt(prompt("¿Cuántas unidades desea llevar?"));
-            validaCantidad = parseFloat(cantidad1);
-            if(!isNaN(validaCantidad)) {
-                valida = true;
-            } else {
-                alert("Ingrese un número válido. Presione 'Aceptar para ingresarlo nuevamente.");
-            }
+const realizarOtraCompraButton = document.getElementById('realizarOtraCompra');
+
+realizarOtraCompraButton.addEventListener('click', reiniciarCompra);
+
+function reiniciarCompra() {
+    pedidoConfirmadoElement.style.display = 'none';
+    carritoCompras.clear();
+    actualizarCarrito();
+    ocultarCarrito();
+    window.location.reload();
+}
+
+const carritosSelect = document.getElementById('carritosSelect');
+const cargarCarritoButton = document.getElementById('cargarCarrito');
+const carritosGuardadosElement = document.getElementById('carritosGuardados');
+
+cargarCarritoButton.addEventListener('click', () => {
+    const carritoSeleccionado = carritosSelect.value;
+    cargarCarritoDesdeLocalStorage(carritoSeleccionado);
+});
+
+
+function cargarCarritoDesdeLocalStorage(carritoSeleccionado) {
+    const carritosGuardados = JSON.parse(localStorage.getItem('carritosGuardados')) || [];
+    const carritoGuardado = carritosGuardados.find(carrito => carrito.nombre === carritoSeleccionado);
+    
+    if (carritoGuardado) {
+        carritoCompras.clear();
+        
+        for (const [producto, cantidad] of Object.entries(carritoGuardado.contenido)) {
+            carritoCompras.set(producto, cantidad);
         }
         
-        llenarCarrito(producto1, cantidad1);
+        actualizarCarrito();
+        ocultarCarrito();
+    }
+}
+
+function guardarCarritoEnLocalStorage() {
+    const carritosGuardados = JSON.parse(localStorage.getItem('carritosGuardados')) || [];
+    
+    const carritoParaGuardar = {
+        nombre: `Carrito ${carritosGuardados.length + 1}`,
+        contenido: {}
+    };
+    
+    carritoCompras.forEach((cantidad, producto) => {
+        carritoParaGuardar.contenido[producto] = cantidad;
+    });
+    
+    carritosGuardados.push(carritoParaGuardar);
+    localStorage.setItem('carritosGuardados', JSON.stringify(carritosGuardados));
+}
+
+function cargarCarritosGuardados() {
+    const carritosGuardados = JSON.parse(localStorage.getItem('carritosGuardados')) || [];
+
+    // Mostrar solo los últimos tres carritos guardados
+    const carritosMostrados = carritosGuardados.slice(-3);
+
+    if (carritosMostrados.length > 0) {
+        carritosSelect.innerHTML = '';
+        carritosMostrados.forEach(carrito => {
+            const option = document.createElement('option');
+            option.textContent = carrito.nombre;
+            carritosSelect.appendChild(option);
+        });
+
+        // Mostrar el select de carritos guardados y el botón de carga
+        carritosGuardadosElement.style.display = 'block';
     } else {
-        return;
+        // Si no hay carritos guardados, mostrar un mensaje
+        carritosSelect.innerHTML = '<option value="">No hay carritos guardados</option>';
     }
 }
 
-function calculaCantidadProductos() {
-    carritoCompras.forEach((valor, clave)=>{
-        cantidadProductos += carritoCompras.get(clave);
-    });
-    return;
-}
+cargarCarritosGuardados();
 
-function calculaPrecioTotal() {
-    carritoCompras.forEach((valor, clave)=>{
-        totalBruto += carritoCompras.get(clave)*productoPrecioBruto.get(clave);
-    });
-    totalNeto = Math.round(totalBruto/1.19);
-    iva = totalBruto - totalNeto;
-    return;
-}
-
-function loop() {
-    alert("Bienvenido al Minimarket de Conejo, presione 'Aceptar' para ver los productos");
-    while(true){
-        let valida = false;
-
-        while(valida == false) {
-            producto = prompt(listaProductos + "\n¿Que desea llevar? (Escriba el nombre del producto tal cómo aparece en el listado.)");
-            validaProducto = parseFloat(producto);
-            if(isNaN(validaProducto) && productoPrecioBruto.has(producto)) {
-                valida = true;
-            } else {
-                alert("Ingrese el nombre del producto que desea llevar, tal como aparece en el listado. Presione 'Aceptar' para mostrar el listado.");
-            }
-        }
-
-        valida = false;
-        while(valida == false){
-            cantidad = parseInt(prompt("¿Cuántas unidades desea llevar?"));
-            validaCantidad = parseFloat(cantidad);
-            if(!isNaN(validaCantidad)) {
-                valida = true;
-            } else {
-                alert("Ingrese un número válido. Presione 'Aceptar para ingresarlo nuevamente.");
-            }
-        }
-
-        llenarCarrito(producto, cantidad);
-        alert("Presione 'Aceptar' para calcular la boleta.");
-        calculaCantidadProductos();
-        calculaPrecioTotal();
-        
-        alert(
-            `
-            Cantidad de productos:  ${cantidadProductos}
-
-            Neto:       ${totalNeto}
-            IVA(19%):   ${iva}
-            Total:      ${totalBruto}
-            `);
-        break;
+cargarCarritoButton.addEventListener('click', () => {
+    const carritoSeleccionado = carritosSelect.value;
+    if (carritoSeleccionado) {
+        cargarCarritoDesdeLocalStorage(carritoSeleccionado);
     }
-}
-
-loop();
-
+});
